b558d860dfb083f50a9400fc718f2769abf5f213
Mike2022SPB <mike.godunoff2020@mail.ru>
2022-07-12 10:40:05 +0300

    Authors added(some fix)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,43 +7,43 @@ class QuestionsController < ApplicationController
 
     @question = Question.new(question_params)
     @question.author = current_user
-    
+
     if @question.save
       redirect_to user_path(@question.user), notice: "Your new question has been created."
     else
-    redirect_to user_path, notice: "The question is not correct."
+    redirect_to user_path(@question.user), notice: "The question is not correct."
     end
   end
-  
+
   def update
     questions_params = params.require(:question).permit(:body, :answer)
 
     @question.update(questions_params)
-    
+
     redirect_to user_path(@question.user), notice: "Your new question has been updated."
   end
-  
+
   def destroy
     @user = @question.user
     @question.destroy
-    
+
     redirect_to user_path(@user), notice: "The question has been deleted."
   end
-  
+
   def show
     @question = Question.find(params[:id])
   end
-  
+
   def index
     @question = Question.new
     @questions = Question.all
   end
-  
+
   def new
     @user = User.find(params[:user_id])
     @question = Question.new(user: @user)
   end
-  
+
   def edit
   end
 
@@ -52,13 +52,13 @@ class QuestionsController < ApplicationController
 
     redirect_to user_path(@question.user), notice: "The question was hid."
   end
-  
+
   private
-  
+
   def ensure_current_user
     redirect_with_alert unless current_user.present?
   end
-  
+
   def set_question_for_current_user
     @question = current_user.questions.find(params[:id])
   end
