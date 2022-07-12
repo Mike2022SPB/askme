5000e7e5d03d17cb4dc208772ebd1554c9906e40
Mike2022SPB <mike.godunoff2020@mail.ru>
2022-07-12 13:58:07 +0300

    Gravatar avatars added

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
   has_many :questions, dependent: :delete_all
   has_many :authored_questions, class_name: "Question", foreign_key: "author_id", dependent: :nullify
 
+  include Gravtastic
+  gravtastic(secure: true, filetype: :png, size: 100, default: "robohash")
+
   def downcase_nickname
     nickname.downcase!
   end
