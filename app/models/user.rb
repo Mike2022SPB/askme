e782a321b9c23c4e3a39562a96c854d597e1c440
Mike2022SPB <mike.godunoff2020@mail.ru>
2022-07-27 15:49:05 +0300

    fixed callback, added method downcase into private, made code according to codestyle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,23 @@
 class User < ApplicationRecord
+  include Gravtastic
+  gravtastic(secure: true, filetype: :png, size: 100, default: "robohash")
+
   has_secure_password
-
-  before_save :downcase_nickname
-
-  validates :email, presence: true, uniqueness: true
-  validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
-
-  validates :nickname, presence: true, uniqueness: true
-  validates :nickname, length: { maximum: 40 }
-  validates :nickname, format: { with: /\A[a-zA-Z0-9_]+\Z/}
-
-  validates :header_color, format: { with: /\A#\h{3}{1,2}\z/ }
 
   has_many :questions, dependent: :delete_all
   has_many :authored_questions, class_name: "Question", foreign_key: "author_id", dependent: :nullify
 
-  include Gravtastic
-  gravtastic(secure: true, filetype: :png, size: 100, default: "robohash")
+  before_validation :downcase_nickname
+
+  validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@.+\.\S+\z/ }
+
+  validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 }, format: { with: /\A[a-zA-Z0-9_]+\Z/ }
+
+  validates :header_color, format: { with: /\A#\h{3}{1,2}\z/ }
+
+  private
 
   def downcase_nickname
-    nickname.downcase!
-  end
-
-  def to_param
-    nickname
+    nickname&.downcase!
   end
 end
