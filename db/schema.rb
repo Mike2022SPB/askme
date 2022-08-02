80b940bdc7d4addad4f62c73d43071757821d667
Mike2022SPB <mike.godunoff2020@mail.ru>
2022-08-02 12:57:43 +0300

    added index to users nickname+uniquness

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_29_105022) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_02_095436) do
   create_table "questions", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_29_105022) do
     t.string "password_digest"
     t.string "header_color", default: "darkred"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
   end
 
   add_foreign_key "questions", "users", column: "author_id"
