497b586be123ba3518c548266fb6000462fe88bb
Mike2022SPB <mike.godunoff2020@mail.ru>
2022-08-30 11:16:33 +0300

    questions_helper fastfix

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,5 @@
 module QuestionsHelper
   def render_with_hashtags(body)
-    body.gsub(Hashtag::HASHTAG_REGEX) { |word| link_to word.downcase, "/hashtags/#{word.downcase.delete('#')}" }.html_safe
+    body.gsub(Hashtag::HASHTAG_REGEX) { |hashtag| link_to hashtag, hashtag_path(hashtag.delete('#').downcase) }.html_safe
   end
 end
